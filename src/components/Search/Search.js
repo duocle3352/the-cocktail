2cca3f5bc98fdc3aa518c1cb536b0cb47fda15f6
duocle3352 <thanhduocddjc@outlook.com>
2023-02-17 21:10:43 +0700

    add search modal and search logic

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,36 +1,130 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import { AiOutlineClose, AiOutlineLoading3Quarters } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
-import config from '~/config';
+
+import { searchService } from '~/services';
+import { useDebounce, useOutsideCloser } from '~/hooks';
+import { ModalWrapper } from '~/components/ModalWrapper';
+import { CloseBtn } from '~/components/CloseBtn';
+import { ProductItem } from '~/components/ProductItem';
+import { SectionHeader } from '~/components/SectionHeader';
 
 function Search() {
+    const [searchValue, setSearchValue] = useState('');
+    const [results, setResults] = useState([]);
+    const [isShowSearchModal, setShowSearchModal] = useState(false);
+    const [isShowLoading, setIsShowLoading] = useState(false);
+    const searchRef = useRef();
+    const inputRef = useRef();
+    const debounceValue = useDebounce(searchValue, 800);
+
+    const toggleShowSearch = () => {
+        setShowSearchModal(!isShowSearchModal);
+    };
+    useOutsideCloser(searchRef, toggleShowSearch);
+
+    useEffect(() => {
+        if (!debounceValue.trim() === '') {
+            setResults([]);
+            return;
+        }
+
+        const fetchApi = async () => {
+            setIsShowLoading(true);
+            const result = await searchService(debounceValue);
+            setIsShowLoading(false);
+            setResults(result);
+        };
+
+        fetchApi();
+    }, [debounceValue]);
+
+    const handleChangeValue = (e) => {
+        const newSearchValue = e.target.value;
+        if (!newSearchValue.startsWith(' ')) {
+            setSearchValue(newSearchValue);
+        }
+    };
+
+    const handleClearValue = () => {
+        setSearchValue('');
+        inputRef.current.focus();
+    };
+
     return (
-        <Link to={config.routes.search} className={`right-tool-style`}>
-            <BiSearch size="1.5rem" />
-        </Link>
+        <>
+            <button className={`right-tool-style`} onClick={toggleShowSearch}>
+                <BiSearch size="1.5rem" />
+            </button>
+
+            {isShowSearchModal && (
+                <ModalWrapper>
+                    <div
+                        className="relative w-[90%] h-[90%] bg-white p-10 overflow-y-scroll"
+                        ref={searchRef}
+                    >
+                        <CloseBtn
+                            icon={<AiOutlineClose size={'1.6rem'} />}
+                            onClose={toggleShowSearch}
+                        />
+
+                        {/* input place */}
+                        <div
+                            className="group relative inline-flex items-center 
+                                        border-2 border-borderColor rounded-md mb-10
+                                        overflow-hidden focus-within:border-primary-green"
+                        >
+                            {/* search icon */}
+                            <BiSearch
+                                size={'1.4em'}
+                                className="w-11 text-darkLightText group-focus-within:text-primary-green"
+                            />
+                            {/* input */}
+                            <input
+                                className="h-11 w-[500px] pr-6"
+                                placeholder="Search"
+                                value={searchValue}
+                                spellCheck={false}
+                                ref={inputRef}
+                                onChange={handleChangeValue}
+                            />
+                            {/* loading */}
+                            {isShowLoading && (
+                                <span className="absolute right-11 top-1/2 -translate-y-1/2">
+                                    <AiOutlineLoading3Quarters className="text-darkLightText animate-spin" />
+                                </span>
+                            )}
+                            {/* clear btn */}
+                            <button
+                                className="mr-2 p-2 text-darkLightText"
+                                onClick={handleClearValue}
+                            >
+                                <AiOutlineClose />
+                            </button>
+                        </div>
+
+                        {/* title */}
+                        {searchValue === '' ? (
+                            <SectionHeader title="Popular products" />
+                        ) : (
+                            <SectionHeader title={searchValue} />
+                        )}
+
+                        {/* search result place */}
+                        {results && results.length > 0 ? (
+                            <div className="grid grid-cols-5 gap-5">
+                                {results.map((result) => (
+                                    <ProductItem key={result.idDrink} item={result} />
+                                ))}
+                            </div>
+                        ) : (
+                            <h5>{`The search "${searchValue}" matches 0 products.`}</h5>
+                        )}
+                    </div>
+                </ModalWrapper>
+            )}
+        </>
     );
 }
 
 export default Search;
-
-// import { useEffect, useState } from 'react';
-// import { searchService } from '~/services';
-// import { useDebounce } from '~/hooks';
-
-// function Search() {
-//     const [result, setResult] = useState([]);
-//     const [searchValue, setSearchValue] = useState('');
-//     const debounceValue = useDebounce(searchValue, 800);
-
-//     useEffect(() => {
-//         const fetchApi = async () => {
-//             const result = await searchService(debounceValue);
-//             setResult(result);
-//         };
-
-//         fetchApi();
-//     }, [debounceValue]);
-
-//     return <h1>Search page</h1>;
-// }
-
-// export default Search;
